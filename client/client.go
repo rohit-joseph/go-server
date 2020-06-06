43b5cd667eca00211cd009782ec3cfc551a943b2
Rohit Joseph <rohitj8.04@gmail.com>
2020-06-05 21:35:39 -0700

    Added initial UDP server and client implementation with some protobuf support

---
+++ client/client.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+	pb "github.com/rohit-joseph/go-server/proto"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a host:port string")
+		return
+	}
+	CONNECT := arguments[1]
+
+	s, err := net.ResolveUDPAddr("udp4", CONNECT)
+	c, err := net.DialUDP("udp4", nil, s)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("The UDP server is %s\n", c.RemoteAddr().String())
+	defer c.Close()
+
+	for {
+		msg := &pb.Msg{}
+		msg.MessageID = []byte{'c', 'b'}
+		msg.Payload = []byte{'d', 'b'}
+		msg.CheckSum = 12
+		out, err := proto.Marshal(msg)
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		text, _ := reader.ReadString('\n')
+		data := []byte(text + "\n")
+		_, err = c.Write(out)
+		if strings.TrimSpace(string(data)) == "STOP" {
+			fmt.Println("Exiting UDP client!")
+			return
+		}
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		buffer := make([]byte, 1024)
+		n, _, err := c.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Reply: %s\n", string(buffer[0:n]))
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	pb "github.com/rohit-joseph/go-server/proto"
+)
+
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a port number!")
+		return
+	}
+	PORT := ":" + arguments[1]
+
+	s, err := net.ResolveUDPAddr("udp4", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	connection, err := net.ListenUDP("udp4", s)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer connection.Close()
+	buffer := make([]byte, 2048)
+	rand.Seed(time.Now().Unix())
+
+	for {
+		n, addr, err := connection.ReadFromUDP(buffer)
+
+		msg := &pb.Msg{}
+		if err := proto.Unmarshal(buffer[0:n], msg); err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(msg.CheckSum)
+
+		data := []byte(strconv.Itoa(random(1, 1001)))
+		fmt.Printf("data: %s\n", string(data))
+		_, err = connection.WriteToUDP(data, addr)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
